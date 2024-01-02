43e816f61e1d47263cd5514eb233ac597816ecb8
djalilhebal <abdeldjalil.hebal@gmail.com>
2024-01-02 19:41:27 +0100

    Add reward levels (Task 5)

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, _
+from odoo import fields, models, api, _
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -8,6 +8,20 @@ class ResPartner(models.Model):
     voyages = fields.One2many('contact_travel.voyage', 'contact_id', string='Voyages')
 
     voyages_count = fields.Integer(string='Nombre de Voyages', compute='_count_voyages')
+
+    # Reward level
+    niv_recomp = fields.Selection(
+        selection=[
+            ('argent', 'Argent'),
+            ('or', 'Or'),
+            ('platine', 'Platine'),
+        ],
+        string='Niveau de récompense',
+        default='argent',
+        compute='_niv_recomp',
+    )
+    # The total trip expenses for the contact, used to in the calculation of the reward level.
+    total_voyage_depense = fields.Float(string='Dépenses totales des voyages (DA)', compute='_total_voyage_depense')
 
     def action_view_partner(self):
         return {
@@ -21,3 +35,23 @@ class ResPartner(models.Model):
     def _count_voyages(self):
         for record in self:
             record.voyages_count = len(record.voyages)
+
+    # This method triggers whenever `total_voyage_depense` changes to automatically update the reward level.
+    @api.depends('total_voyage_depense')
+    def _niv_recomp(self):
+        for contact in self:
+            if contact.total_voyage_depense >= 100_000:
+                contact.niv_recomp = 'platine'
+            elif contact.total_voyage_depense >= 50_000:
+                contact.niv_recomp = 'or'
+            else:
+                contact.niv_recomp = 'argent'
+
+    # This method triggers whenever `voyages_count` or `voyages.prix` changes to automatically update `total_voyage_depense`.
+    # Why depend on `voyages_count`? To comply with the requirement:
+    # "Ce niveau de récompense doit être mis à jour chaque fois qu'un nouveau voyage est associé à un client."
+    @api.depends('voyages_count', 'voyages.prix')
+    def _total_voyage_depense(self):
+        for contact in self:
+            total_depense = sum(contact.voyages.mapped('prix'))
+            contact.total_voyage_depense = total_depense
