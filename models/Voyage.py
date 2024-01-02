c4cbc4d40451536f40d46793cbf852667c0e0ad2
djalilhebal <abdeldjalil.hebal@gmail.com>
2024-01-02 19:47:39 +0100

    Update Voyage to make all fields required

--- models/Voyage.py
+++ models/Voyage.py
@@ -5,8 +5,8 @@ from odoo import fields, models, _
 class Voyage(models.Model):
     _name = 'contact_travel.voyage'
 
-    nom = fields.Char(string="Nom du voyage")
-    date_depart = fields.Date(string="Date depart")
-    destination = fields.Char(string="Destination")
-    prix = fields.Float(string="Prix (DA)")
-    contact_id = fields.Many2one("res.partner", string="Contact")
+    nom = fields.Char(string="Nom du voyage", required=True)
+    date_depart = fields.Date(string="Date depart", required=True)
+    destination = fields.Char(string="Destination", required=True)
+    prix = fields.Float(string="Prix (DA)", required=True)
+    contact_id = fields.Many2one("res.partner", string="Contact", required=True)
